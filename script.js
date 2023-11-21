67e26ef64b4a4ed060b7f769e2d31697f604213f
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-11-21 17:25:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //your JS code here. If required.
  document.addEventListener("DOMContentLoaded", function() {
-            // Get the body element
+            
             var body = document.body;
 
-            // Check if the body is empty
+           
             if (body.children.length === 0) {
-                // Create a new p element
+                
                 var paragraph = document.createElement("p");
 
-                // Set the text content of the p element
+             
                 paragraph.textContent = "DOM load success";
 
-                // Append the p element to the body
+               
                 body.appendChild(paragraph);
             }
         });
