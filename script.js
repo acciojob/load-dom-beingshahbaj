8d2d16c4562c5eaf12b455d8be779fca19cf1f47
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-11-21 18:57:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOMContentLoaded event fired");
-
-    var body = document.body;
-
-    if (body.children.length === 0) {
-        console.log("Body has no children");
-
-        var paragraph = document.createElement("p");
-        paragraph.textContent = "DOM load success";
-
-        body.appendChild(paragraph);
-    } else {
-        console.log("Body already has children:", body.children);
+document.addEventListener('DOMContentLoaded', (event) => {
+    
+    if (document.body.innerHTML.trim() === '') {
+       
+        var para = document.createElement('p');
+        
+       success"
+        para.textContent = 'DOM load success';
+        
+        
+        document.body.appendChild(para);
     }
 });
